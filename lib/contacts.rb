ec5a6baeaf4ee891c7bc4326d8aaf9159f21d164
clean-command-1851 <ed7901979t@gmail.com>
2019-08-22 01:33:32 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -1,11 +1,11 @@
 require 'pry'
 
   # This is the array we will be passing into the remove_strawberry method
-  # contacts = {
-  #   "Jon Snow" => {
-  #     name: "Jon",
-  #     email: "jon_snow@thewall.we", 
-  #     favorite_ice_cream_flavors: ["chocolate", "vanilla"]
+   contacts = {
+    "Jon Snow" => {
+       name: "Jon",
+       email: "jon_snow@thewall.we", 
+       favorite_ice_cream_flavors: ["chocolate", "vanilla"]
    },
      "Freddy Mercury" => {
        name: "Freddy",
@@ -16,5 +16,9 @@ require 'pry'
   
 def remove_strawberry(contacts)
   contact.each do |person, contact_details_hash|
-    binding.pry
+    if person == "Freddy Mercury"
+      contact.details.hash.each do |attribute, data|
+        binding.pry
+      end
+    end
 end
